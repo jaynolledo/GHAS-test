28dfa75d8a7f51c05565dc750e17f3055646ad0d
Jay Nolledo <98284623+jaynolledo@users.noreply.github.com>
2024-06-12 15:26:10 -0500

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-txtUserId = getRequestString("UserId");
-sql = "SELECT * FROM Customers WHERE CustomerId = @0";
-command = new SqlCommand(sql);
-command.Parameters.AddWithValue("@0",txtUserID);
-command.ExecuteReader();
